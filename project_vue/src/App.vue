3513539a49b81dd31f1cf115b4e4241192f7a0e7
rusky23 <matthew.morin@sjsu.edu>
2018-05-04 16:23:28 -0700

    Changed company name to CodeMode

--- project_vue/src/App.vue
+++ project_vue/src/App.vue
@@ -4,7 +4,7 @@
 
     <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
 
-    <b-navbar-brand to="/">Company Name</b-navbar-brand>
+    <b-navbar-brand to="/">CodeMode</b-navbar-brand>
 
     <b-collapse is-nav id="nav_collapse">
 

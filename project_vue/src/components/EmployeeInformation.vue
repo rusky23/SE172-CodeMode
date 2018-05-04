dbeab2a2135c82fdb1450cfa930b0dce834e4612
payam9 <payamkhosravi4@gmail.com>
2018-05-04 12:58:40 -0700

    fix ligin with okta

--- project_vue/src/components/EmployeeInformation.vue
+++ project_vue/src/components/EmployeeInformation.vue
@@ -1,7 +1,20 @@
 <template>
-  <div class="container-fluid mt-4">
-    <h1 class="headline">Employee Information</h1>
-    <h3>Welcome to Your page "Employee name"</h3>
-    <h1>Only authenticate user can see this page</h1>
+  <div class="div">
+    <div class="container-fluid mt-4">
+      <h1 class="headline">Employee Information</h1>
+      <h3>Welcome to Your page "Employee name"</h3>
+      <h1>Only authenticate user can see this page</h1>
+    </div>
   </div>
 </template>
+
+<style>
+.div {
+  height: 90vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  background-color: #f7f9f9;
+}
+</style>

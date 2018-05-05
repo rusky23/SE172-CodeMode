b2377dfaebce520376d89571862140896b271cc8
payam9 <payamkhosravi4@gmail.com>
2018-05-04 19:05:35 -0700

    test the git update

--- project_vue/src/components/EmployeeInformation.vue
+++ project_vue/src/components/EmployeeInformation.vue
@@ -3,7 +3,7 @@
     <div class="container-fluid mt-4">
       <h1 class="headline">Employee Information</h1>
       <h3>Welcome to Your page "Employee name"</h3>
-      <h1>Only authenticate user can see this page</h1>
+      <h1>Only Authenticate User can see This page--</h1>
     </div>
   </div>
 </template>
